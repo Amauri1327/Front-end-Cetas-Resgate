3e8b15caefa968cab9c7090f7f3450c605969e78
Amauri1327 <amauri1816@hotmail.com>
2024-11-17 20:42:57 -0300

    criacao botao adicionar e relatorio

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import { MdOutlineAddBox } from "react-icons/md"
+import React from "react";
+import { MdOutlineAddBox } from "react-icons/md";
+import { HiOutlineDocumentReport } from "react-icons/hi";
 
 export const Menu = () => {
+  const handlePress = () => {
+    console.log("Click");
+  };
+
   return (
-    <div className='flex justify-center items-center mt-5'>
-        <div className='text-center flex justify-center items-center gap-10 bg-red-200 h-16 max-w-2xl'>
-            <div className='bg-blue-100'>cadastrar</div>
-            <div className='bg-green-50'>Relatorio</div>
+    <div className="flex justify-center items-center mt-10">
+      <div className="text-center flex justify-center items-center gap-10 h-16 max-w-2xl">
+        <div className="bg-blue-100">
+          <button onClick={handlePress}>
+            <MdOutlineAddBox size={90} />
+            cadastrar
+          </button>
         </div>
+        <div className="bg-green-50">
+          <button onClick={handlePress}>
+            <HiOutlineDocumentReport size={90} /> Relatorio
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Menu;
