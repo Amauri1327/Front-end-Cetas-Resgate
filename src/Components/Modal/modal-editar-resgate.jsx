e6ab70d11d235bd4a24d7857d6a1c57f605865d9
Amauri1327 <amauri1816@hotmail.com>
2025-01-12 15:59:58 -0300

    adicao dos campos: quantidade e origem no modal-editar-resgate

--- src/Components/Modal/modal-editar-resgate.jsx
+++ src/Components/Modal/modal-editar-resgate.jsx
@@ -10,7 +10,9 @@ const EditModal = ({ show, handleClose, id, handleEdit }) => {
     city: "",
     data: "",
     animalSituation: "",
-    animalDestination: ""
+    animalDestination: "",
+    animalQuantity: "",
+    origin: "",
   });
 
   // Buscar dados do resgate a partir do id quando o modal for aberto
@@ -28,7 +30,9 @@ const EditModal = ({ show, handleClose, id, handleEdit }) => {
             city: data.city || "",
             data: data.data || "",
             animalSituation: data.animalSituation || "",
-            animalDestination: data.animalDestination || ""
+            animalDestination: data.animalDestination || "",
+            animalQuantity: data.animalQuantity || "",
+            origin: data.origin || "",
           });
         })
         .catch((err) => console.error("Erro ao buscar dados: ", err));
@@ -65,126 +69,185 @@ const EditModal = ({ show, handleClose, id, handleEdit }) => {
       <div className="bg-white p-4 rounded-lg shadow-lg w-1/2">
         <h2 className="text-xl font-bold mb-4">Editar Resgate</h2>
         <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Solicitante</label>
-            <input
-              type="text"
-              value={formData.applicant}
-              onChange={(e) =>
-                setFormData({ ...formData, applicant: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          {/* Adicione os outros campos aqui... */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">
+                Solicitante
+              </label>
+              <input
+                type="text"
+                value={formData.applicant}
+                onChange={(e) =>
+                  setFormData({ ...formData, applicant: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+            {/* Adicione os outros campos aqui... */}
 
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Telefone</label>
-            <input
-              type="text"
-              value={formData.phoneApplicant}
-              onChange={(e) =>
-                setFormData({ ...formData, phoneApplicant: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Espécie</label>
-            <input
-              type="text"
-              value={formData.specie}
-              onChange={(e) =>
-                setFormData({ ...formData, specie: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Endereço</label>
-            <input
-              type="text"
-              value={formData.address}
-              onChange={(e) =>
-                setFormData({ ...formData, address: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Bairro</label>
-            <input
-              type="text"
-              value={formData.neighborhood}
-              onChange={(e) =>
-                setFormData({ ...formData, neighborhood: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Cidade</label>
-            <input
-              type="text"
-              value={formData.city}
-              onChange={(e) =>
-                setFormData({ ...formData, city: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Data</label>
-            <input
-              type="text"
-              value={formData.data}
-              onChange={(e) =>
-                setFormData({ ...formData, data: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Situação do Animal</label>
-            <input
-              type="text"
-              value={formData.animalSituation}
-              onChange={(e) =>
-                setFormData({ ...formData, animalSituation: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2">Destino do Animal</label>
-            <input
-              type="text"
-              value={formData.animalDestination}
-              onChange={(e) =>
-                setFormData({ ...formData, animalDestination: e.target.value })
-              }
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Telefone</label>
+              <input
+                type="text"
+                value={formData.phoneApplicant}
+                onChange={(e) =>
+                  setFormData({ ...formData, phoneApplicant: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Espécie</label>
+              <input
+                type="text"
+                value={formData.specie}
+                onChange={(e) =>
+                  setFormData({ ...formData, specie: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Endereço</label>
+              <input
+                type="text"
+                value={formData.address}
+                onChange={(e) =>
+                  setFormData({ ...formData, address: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Bairro</label>
+              <input
+                type="text"
+                value={formData.neighborhood}
+                onChange={(e) =>
+                  setFormData({ ...formData, neighborhood: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Cidade</label>
+              <input
+                type="text"
+                value={formData.city}
+                onChange={(e) =>
+                  setFormData({ ...formData, city: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Data</label>
+              <input
+                type="date"
+                value={formData.data}
+                onChange={(e) =>
+                  setFormData({ ...formData, data: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
 
+            <div>
+              <label className="block text-sm font-medium mb-2">
+                Quantidade
+              </label>
+              <input
+                type="number"
+                value={formData.animalQuantity}
+                onChange={(e) =>
+                  setFormData({ ...formData, animalQuantity: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
 
-          <div className="flex justify-end">
-            <button
-              type="button"
-              onClick={handleClose}
-              className="mr-4 bg-gray-500 text-white px-4 py-2 rounded"
-            >
-              Cancelar
-            </button>
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-              Salvar
-            </button>
-          </div>
+            <div>
+              <label className="block text-sm font-medium mb-2">Origem</label>
+              <select
+                type="text"
+                name="origin"
+                value={formData.origin}
+                onChange={(e) =>
+                  setFormData({ ...formData, origin: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              >
+                <option value="" disabled hidden className="text-gray-400">
+                  Selecione uma opção
+                </option>
+                <option value="apreensao" className="text-gray-900">
+                  Apreensão
+                </option>
+                <option value="resgate" className="text-gray-900">
+                  Resgate
+                </option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">
+                Situação do Animal
+              </label>
+              <input
+                type="text"
+                value={formData.animalSituation}
+                onChange={(e) =>
+                  setFormData({ ...formData, animalSituation: e.target.value })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">
+                Destino do Animal
+              </label>
+              <input
+                type="text"
+                value={formData.animalDestination}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    animalDestination: e.target.value,
+                  })
+                }
+                className="w-full p-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+
+            <div className="flex justify-end">
+              <button
+                type="button"
+                onClick={handleClose}
+                className="mr-4 bg-gray-500 text-white px-4 py-2 rounded"
+              >
+                Cancelar
+              </button>
+              <button
+                type="submit"
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Salvar
+              </button>
+            </div>
           </div>
         </form>
       </div>
-      
     </div>
   );
 };
